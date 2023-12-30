599a1dc912a9f136e5ee7c0a1df9afde336c1165
anadykhan <aiyoun.khan@gmail.com>
2023-12-30 20:32:09 +0600

    Project started

---
+++ DataAccessLayer/Models/Room.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Models
+{
+    public class Room
+    {
+        [Key]
+        public int RoomID { get; set; }
+        public int RoomNumber { get; set; }
+        public string RoomType { get; set; }
+        public string AvailabilityStatus { get; set; }
+        public string ImageURL { get; set; }
+    }
+}
